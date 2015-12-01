b7ac79e9f302ce81325755b4e109c2b18bb9ac77
Cyaan <efecftw@gmail.com>
2015-12-01 11:42:01 +0000

    modified baby.hs

--- BabysFirstFunctions/baby.hs
+++ BabysFirstFunctions/baby.hs
@@ -1,2 +1,10 @@
+--Function that takes a parameter
 doubleMe x = x + x
-doubleUs x y = x * 2 + y * 2
+--Funtcion with Functions
+doubleUs x y = doubleMe x + doubleMe y
+--if, then, else are expressions
+doubleSmallNumber x = if x > 100 then x else x * 2
+--Functions can not start with an uppercase letter but can use ' in our funcion names
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+--Functions without parameters are defined as defintions
+it'sMario = "It's a-me, Mario"

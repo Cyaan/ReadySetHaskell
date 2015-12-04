c962793c403cdfeb81cffa2235b97aaba9169e62
Cyaan <efecftw@gmail.com>
2015-12-04 19:19:22 +0000

    Modified Baby.hs and added Start.hs

--- BabysFirstFunctions/baby.hs
+++ BabysFirstFunctions/baby.hs
@@ -1,6 +1,6 @@
 --Function that takes a parameter
 doubleMe x = x + x
---Funtcion with Functions
+--Function with Functions
 doubleUs x y = doubleMe x + doubleMe y
 --if, then, else are expressions
 doubleSmallNumber x = if x > 100 then x else x * 2
---
+++ ReadySetGo/Start.hs
@@ -0,0 +1,31 @@
+--Simple arithmetic
+addition = 2 + 15
+multiplication = 49 * 100
+substraction = 18 - 1472
+division = 5 / 2
+--More operators can be used on one line and all precedence rules are obeyed
+example1 = (50 * 100) - 100
+example2 = 50 * 100 - 4999
+example3 = 50 * (100 - 4999)
+--When negating numbers, use parentheses
+--negativeMultiplyF = 5 * -3
+negativemultyplyT = 5 * (-3)
+
+--Boolean algebra
+-- & twice means and
+booland1 = True && False
+booland2 = True && True
+-- | twice means or
+boolor = False || True
+-- Not negates True and False
+boolnot = not False
+-- You can also use more boolean operators
+boolandnot = not (True && False)
+--equality
+equalT = 5 == 5
+equalF = 1 == 0
+notequalT = 5 /= 4
+notequalF = 5 /= 5
+stringequal = "Hello" == "Hello"
+
+
